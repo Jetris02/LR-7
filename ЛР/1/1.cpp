33a4d956f0da96298a81eec0668a6c925ece24eb
Jetris02 <86570861+Jetris02@users.noreply.github.com>
2021-07-05 11:36:49 +0300

    Add files via upload

---
+++ ЛР/1/1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double avg_harm(double x, double y)
+{
+    double avg;
+    x = ~(int)x;
+    y = ~(int)y;
+    avg = (x + y) / 2;
+    avg = ~(int)avg;
+    return avg;
+}
+
+void main()
+{
+    setlocale(LC_ALL, "Russian");
+    double doub_1, doub_2;
+    cout << "Введите 2 числа\n";
+    cin >> doub_1;
+    cin >> doub_2;
+    cout << "Среднее гармоническое значеие " << doub_1 << " и " << doub_2 << " = " << avg_harm(doub_1, doub_2) << endl;
+    system("pause");
+}
---
+++ ЛР/2/2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void sort(double& x, double& y, double& z)
+{
+    double arr[3] = { x, y , z };
+    double min = arr[0], max = arr[0], beet;
+    for (int i = 0; i < 3; i++)
+    {
+        if (arr[i] < min)
+            min = arr[i];
+        if (arr[i] > max)
+            max = arr[i];
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        if (arr[i] != min)
+        {
+            if (arr[i] != max)
+                beet = arr[i];
+        }
+    }
+    x = min;
+    z = max;
+    y = (min + max + beet) / 3;
+}
+
+void main()
+{
+    setlocale(LC_ALL, "Russian");
+    double doub_1 = 15.78, doub_2 = 43.51, doub_3 = 24.8;
+    cout << doub_1 << " " << doub_2 << " " << doub_3 << endl;
+    sort(doub_1, doub_2, doub_3);
+    cout << "Наименьшее = " << doub_1 << ", среднее = " << doub_2 << ", наибольшее = " << doub_3 << endl;
+    system("pause");
+}
